e31a66ab58c63bfd3b0196a5697e2e0474c18ec1
awadi99 <adityawaghmare9990@gmail.com>
2026-04-11 13:04:34 +0530

    Initial Frontend Process

---
+++ Frontend/src/pages/Landing.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Navbar from '../components/common/Navbar';
+import Hero from '../components/sections/Hero';
+import Problem from '../components/sections/Problem';
+import Solution from '../components/sections/Solution';
+import Features from '../components/sections/Features';
+import HowItWorks from '../components/sections/HowItWorks';
+import WhySkillPro from '../components/sections/WhySkillPro';
+import FAQ from '../components/sections/FAQ';
+import CTA from '../components/sections/CTA';
+import Footer from '../components/common/Footer';
+
+
+export default function Landing() {
+    return (
+        <div>
+            <Navbar />
+            <Hero />
+            <Problem />
+            <Solution />
+            <Features />
+            <HowItWorks />
+            <WhySkillPro />
+            <FAQ />
+            <CTA />
+            <Footer />
+        </div>
+    )
+}
