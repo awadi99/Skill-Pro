48f65f46e1f02c82abb3dea5128b0e313e2ee9a3
awadi99 <adityawaghmare9990@gmail.com>
2026-04-19 20:09:48 +0530

    mt-15

--- Frontend/src/layouts/MainLayout.jsx
+++ Frontend/src/layouts/MainLayout.jsx
@@ -4,12 +4,12 @@ import Sidebar from "../pages/Sidebar";
 
 export default function MainLayout() {
     return (
-        <div className="flex  min-h-screen bg-[#020617] text-white">
+        <div className="flex  min-h-screen bg-[#020617] text-white  ">
 
 
             <Sidebar />
-            <div className="flex-1 flex flex-col min-w-0"> {/* min-w-0 is critical for flex child responsiveness */}
-                <main className="flex-1 p-4 sm:p-6 md:p-10 w-full max-w-[1600px] mx-auto">
+            <div className="flex-1 flex flex-col min-w-0"> 
+                <main className="flex-1 p-4 sm:p-6 md:p-10 w-full max-w-[1600px] mx-auto mt-15">
                     <Outlet />
                 </main>
             </div>
